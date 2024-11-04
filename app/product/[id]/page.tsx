c3ae4bf1bca2dff338acdf0ed85df7c13b074f45
Oldsmob1le <abdulin00@vk.com>
2024-11-05 00:28:51 +0300

    first commit1

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -11,25 +11,23 @@ async function fetchProduct(id: string): Promise<Product> {
   return res.json();
 }
 
-interface ProductPageProps {
-  params: { id: string };
-}
-
-const ProductPage = async ({ params }: ProductPageProps) => {
-  const product = await fetchProduct(params.id);
-
-  return (
-    <div className="p-4">
-      <img
-        src={product.image}
-        alt={product.title}
-        className="w-full h-96 object-contain"
-      />
-      <h1 className="text-2xl font-bold mt-4">{product.title}</h1>
-      <p className="text-gray-700 mt-2">{product.description}</p>
-      <p className="text-lg font-semibold mt-2">${product.price}</p>
-    </div>
-  );
-};
-
+const ProductPage = async ({ params }: { params: { id: string } }) => {
+    const { id } = await params;
+  
+    const product = await fetchProduct(id);
+  
+    return (
+      <div className="p-4">
+        <img
+          src={product.image}
+          alt={product.title}
+          className="w-full h-96 object-contain"
+        />
+        <h1 className="text-2xl font-bold mt-4">{product.title}</h1>
+        <p className="text-gray-700 mt-2">{product.description}</p>
+        <p className="text-lg font-semibold mt-2">${product.price}</p>
+      </div>
+    );
+  };
+  
 export default ProductPage;
